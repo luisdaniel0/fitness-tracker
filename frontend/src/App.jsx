689b2edf7713cf49fe00486bc5c43b929227da15
Luis Sanchez <luisanchezdh@gmail.com>
2023-03-28 19:13:17 -0400

    added navbar component, styled app.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <div className="App">
-      Hello my name is Luis Sanchez
+      <BrowserRouter>
+        <Navbar />
+        <div className='pages'>
+          <Routes>
+            <Route
+              path='/'
+              element={<Home/>} />
+          </Routes>
+        </div>
+        </BrowserRouter>
       </div>
   )
 }
