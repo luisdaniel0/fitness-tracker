05c4d48ddff78d26ab57fb36b8df6f87e572a487
Luis Sanchez <luisanchezdh@gmail.com>
2023-03-28 20:30:06 -0400

    installed cors, fixed server.js, fetched api to display workout information in frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+  
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('http://localhost:4000/api/workouts/')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  },[])
+
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className='workouts'>
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
+      </div>
+    </div>
   )
 }
 
