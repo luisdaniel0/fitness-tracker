b12527babb4509b468c5e0fae00347f25531efb9
Luis Sanchez <luisanchezdh@gmail.com>
2023-03-29 12:33:02 -0400

    workout form created, styled

--- server/server.js
+++ server/server.js
@@ -5,8 +5,11 @@ const workoutRoutes = require('./routes/workouts')
 const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
